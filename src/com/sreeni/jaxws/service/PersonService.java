067a0f0e42115ae641378db7d5244ea5a8c30e0b
sreenivasula mula <sreenivasula.mula@capitalone.com>
2017-07-31 22:53:32 -0400

    Initial commit

---
+++ src/com/sreeni/jaxws/service/PersonService.java
@@ -0,0 +1,14 @@
+package com.sreeni.jaxws.service;
+
+import com.sreeni.jaxws.beans.Person;
+
+public interface PersonService {
+
+    public boolean addPerson(Person p);
+    
+    public boolean deletePerson(int id);
+    
+    public Person getPerson(int id);
+    
+    public Person[] getAllPersons();
+}
